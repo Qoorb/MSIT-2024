7651961bf8ab53a06b422099e5e38aecc8ac3ce2
Qoorb <rborn3243@gmail.com>
2024-03-02 22:33:14 +0700

    fix dataset.py

--- dataset.py
+++ dataset.py
@@ -2,11 +2,17 @@ import os
 
 import pickle
 
-import numpy as np
-
+import torch
 from torch.utils.data import Dataset
 
 from scipy.io import loadmat
+
+
+'''
+TODO:
+    The dataset class should contain data check?
+    Add GPU processing?
+'''
 
 
 class EEGDataset(Dataset):
@@ -14,17 +20,30 @@ class EEGDataset(Dataset):
         self.save_path = save_path
         self.dir_files = dir_files
         self.transform = transform
-        # self.files = [file for file in os.listdir(dir_files) if file.endswith('.mat')]
-        # self.data = self._read_data()
-        self.data = []
+        self.data = self._read_data()
     
+    def __len__(self) -> int:
+        return len(self.data)
+    
+    def __getitem__(self, idx) -> torch.Tensor:
+        sample = self.data[idx]
+
+        if self.transform:
+            sample = self.transform(sample)
+            
+        return torch.tensor(sample, dtype=torch.float32).to(device)
+
     # inner function
+    def _info(self) -> tuple:
+        return len(self.data), self.data[0].shape
+
     def _read_data(self) -> list:
+        files = [file for file in os.listdir(self.dir_files) if file.endswith('.mat')]
         dataset = []
 
-        for file in self.files:
-            data = loadmat(os.path.join(self.dir_files, file))['EEG'][0][0][15][:64] # 64 - EEG channels
-            dataset.append(np.array(data))
+        for file in files:
+            data = loadmat(os.path.join(self.dir_files, file))['EEG'][0][0][15][:63] # 63 - EEG channels
+            dataset.append(data)
 
             print(f"{file} added")
         
@@ -32,26 +51,24 @@ class EEGDataset(Dataset):
     
     def save(self) -> None:
         pickle.dump(self, open(f"{self.save_path}/dataset_EEG.pkl", 'wb'), True)
-        # np.savetxt(f"{self.save_path}/dataset_EEG.csv", self.data, delimiter=",", fmt='%.3f')
 
         print('dataset has been saved')
 
     @staticmethod
-    def load_data(file_name: str) -> list:
+    def load_dataset(file_name: str) -> list:
         data = pickle.load(open(file_name,'rb'))
-        # self.data = np.loadtxt(file_name)
         
         print('dataset has been loaded')
         return data
 
 
 if __name__ == '__main__':
-    # data = EEGDataset(dir_files='./data/d002')
-    # data.save()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    # data = pickle.load(open("data/dataset_EEG.pkl",'rb'))
-    # print(data.data)
+    data = EEGDataset(dir_files='./data/d002')
+    data.save()
 
-    data = EEGDataset().load_data("data/dataset_EEG.pkl")
-    print(data.data)
+    # data = EEGDataset.load_dataset("./data/dataset_EEG.pkl")
+    # print(data._info())
+    
     print('0')
